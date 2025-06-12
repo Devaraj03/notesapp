92ccbc0435d355d6b66589b1c935a8d97bffe93b
Devaraj03 <ddevaraj120@gmail.com>
2025-06-13 04:25:52 +0530

    Updated project with fixes

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.sqlite');
+const better_sqlite3 = require('better-sqlite3').verbose();
+const db = new better_sqlite3.Database('./database.sqlite');
 
 db.serialize(() => {
   db.run(`
